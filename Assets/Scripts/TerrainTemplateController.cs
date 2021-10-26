d602281422ada2e1cae318524cfba013f703abf3
luckynee <janspurba64@gmail.com>
2021-10-27 01:32:16 +0700

    DILo-endless-run

---
+++ Assets/Scripts/TerrainTemplateController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainTemplateController : MonoBehaviour
+{
+    private const float debugLineHeight = 10.0f;
+
+    private void onDrawGizmos()
+    {
+        Debug.DrawLine(transform.position + Vector3.up * debugLineHeight / 2, transform.position + Vector3.down * debugLineHeight / 2, Color.green);
+    }
+}
